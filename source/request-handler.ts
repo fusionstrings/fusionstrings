e50d0d502bae2fa8364549d905d25c6475a617ae
Dilip Kr. Shukla <fusionstrings@gmail.com>
2024-10-01 23:23:07 +0200

    improvements (#15)

--- source/request-handler.ts
+++ source/request-handler.ts
@@ -1,6 +1,16 @@
 import { serveFile } from "@std/http";
 import browserImportmap from '#browser-importmap' with { type: 'json' };
+import serverImportmap from '#server-importmap' with { type: 'json' };
+
 type BrowserAssets = keyof typeof browserImportmap['imports'];
+type ServerAssets = keyof typeof serverImportmap['imports'];
+
+async function notFound() {
+    const templateURL = new URL('./templates/404.html', import.meta.url).toString();
+    const notFound = await fetch(templateURL);
+
+    return new Response(notFound.body, { status: 404 });
+}
 
 async function requestHandler(request: Request): Promise<Response> {
     try {
@@ -19,15 +29,19 @@ async function requestHandler(request: Request): Promise<Response> {
             return fetch(resourcePath)
         }
 
-        const { requestHandlerHTTP } = await import(pathnameHandler);
-        return requestHandlerHTTP(request);
+        const serverPath = pathnameHandler.replace('#', './')
+
+        if (serverPath in serverImportmap.exports) {
+            const resourcePath = serverImportmap.exports[serverPath as ServerAssets]
+            const resourceURL = new URL(resourcePath, import.meta.url)
+            const { requestHandlerHTTP } = await import(resourceURL.toString());
+            return requestHandlerHTTP(request);
+        }
+        return notFound()
     } catch (error) {
         console.error(error.message || error.toString());
 
-        const templateURL = new URL('./templates/404.html', import.meta.url).toString();
-        const notFound = await fetch(templateURL);
-
-        return new Response(notFound.body, { status: 404 });
+        return notFound()
     }
 }
 
